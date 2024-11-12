16d3633fa8c90668f3b93aa82d01942c33da918f
Eng/Asmaa Elsayed <asmaariad883@gmail.com>
2024-11-12 19:26:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+
+
+
+
+
+const button = document.getElementById('myButton');
+const tooltip = document.getElementById('tooltip');
+button.addEventListener('click', () => {
+    alert('AsmaaSayed-code.github.io says!\n تم نسخ النص');  
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const button1 = document.getElementById('myButton2');
+const tooltip1 = document.getElementById('tooltip2');
+button1.addEventListener('click', () => {
+    alert('AsmaaSayed-code.github.io says!\n تم نسخ النص');  
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip2"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const button2 = document.getElementById('myButton3');
+const tooltip2 = document.getElementById('tooltip3');
+button2.addEventListener('click', () => {
+    alert('AsmaaSayed-code.github.io says!\n تم نسخ النص');  
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip3"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+
+
+
